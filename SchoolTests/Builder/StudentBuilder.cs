2d87b7322775d9ed297012977b5f8cb948b138dd
Dell <Dell@DESKTOP-TK7GSO1>
2023-05-18 21:36:13 -0300

    feat: Student

---
+++ SchoolTests/Builder/StudentBuilder.cs
@@ -0,0 +1,44 @@
+﻿using SchoolAPI.Data.Model;
+using SchoolTests.Interfaces;
+
+namespace SchoolTests.Builder;
+
+public class StudentBuilder : IBuilder<Student>
+{
+    private Student obj;
+
+    public StudentBuilder()
+    {
+        Build();
+    }
+    public int Id => 1;
+
+    public void Build()
+    {
+        obj = new Student(Id, "Welligton", "D3386E9", "well2023", "well2023", 1, true);
+    }
+
+    public int Delete() => Id;
+
+    public async Task<Student> Get()
+    {
+        obj.Id = Id;
+        return obj;
+    }
+
+    public async Task<ICollection<Student>> GetAll()
+    {
+        ICollection<Student> student = new List<Student>();
+        student.Add(obj);
+
+        return student;
+    }
+
+    public Student Post() => obj;
+
+    public Student Put()
+    {
+        obj.Id = Id;
+        return obj;
+    }
+}
---
+++ SchoolTests/Business/StudentBusiness.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using SchoolAPI.Business.Services;
+using SchoolAPI.Data.Repository;
+using SchoolTests.Builder;
+using SchoolTests.Interfaces;
+
+namespace SchoolTests.Business;
+
+public class StudentBusiness: ITests
+{
+    private StudentService service;
+    private StudentBuilder builder;
+    public StudentBusiness()
+    {
+        service = new StudentService(new Mock<IStudentRepository>().Object);
+        builder = new StudentBuilder();
+    }
+
+    public async Task<bool> Delete() => await service.Delete(builder.Delete());
+
+    public string DeleteByIdZero() => service.Delete(0).Exception.InnerException.Message;
+
+    public async Task<int> GetAll()
+    {
+        var obj = builder.GetAll();
+
+        var repository = new Mock<IStudentRepository>();
+
+        repository.Setup(x => x.GetAll()).Returns(obj);
+
+        service = new StudentService(repository.Object);
+
+        return service.GetAll().Result.Count;
+    }
+
+    public async Task<bool> GetById()
+    {
+        var obj = builder.Get();
+
+        var repository = new Mock<IStudentRepository>();
+
+        repository.Setup(x => x.GetById(obj.Id)).Returns(obj);
+
+        service = new StudentService(repository.Object);
+
+        return service.GetById(obj.Result.Id).Id > 0;
+    }
+
+    public string GetByIdZero() => service.GetById(0).Exception.InnerException.Message;
+
+    public Task<bool> Post() => service.Post(builder.Post());
+
+    public Task<bool> Put() => service.Put(builder.Put());
+}
